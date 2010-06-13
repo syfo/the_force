1f7028cc43a96ca9082e891643e523d288289c65
Ryan Ziegler <ryan@symbolforce.com>
2010-06-12 22:22:14 -0400

    including mapping array by default

--- lib/the_force.rb
+++ lib/the_force.rb
@@ -2,5 +2,6 @@ ROOT = File.join(File.dirname(__FILE__), 'the_force')
 require File.join(ROOT, 'keep_trying')
 require File.join(ROOT, 'thread_pool')
 require File.join(ROOT, 'timer')
+require File.join(ROOT, 'mapping_array')
 #require File.join(ROOT, 'object_support')
 true
