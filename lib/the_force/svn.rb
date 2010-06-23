0e2888e069d7755880de36a75ed7bf1fba2fbca3
Ryan Ziegler <ryan@symbolforce.com>
2010-06-23 05:26:06 -0400

    general use svn revision methods

--- lib/the_force/svn.rb
+++ lib/the_force/svn.rb
@@ -1,9 +1,16 @@
 module TheForce
-  def svn_revision(file = "#{RAILS_ROOT}/revision.txt")
-    rev = (/\$Rev: (\d*)\$/ =~ open(file).read)[1]
-    Rails.logger "No revision found in #{file}"
-    rev
-  rescue FileNotFoundError
-    Rails.logger.error "#{file} not found to pull svn revision from" if defined? Rails
+  def svn_revision(file = nil)
+    (svn_revision_from_file if file) || svn_revision_from_repo
+  end
+
+  #expects file created with svn:keywords  
+  def svn_revision_from_file(file)
+    m = (/\$Rev: (\d*)\$/ =~ open(file).read)
+    m and m[1]
+  end
+  
+  def svn_revision_from_repo
+    m = `svn info`.match(/Last Changed Rev:\s*(\d*)$/)
+    m and m[1]
   end
 end
